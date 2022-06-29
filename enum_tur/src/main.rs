b8bbe09f17903da9d99bc56cd59457a563e83f33
tylerleonard5 <tylerleonard@ufl.edu>
2022-06-28 23:54:48 -0400

    done

--- enum_tur/src/main.rs
+++ enum_tur/src/main.rs
@@ -1,10 +1,31 @@
-fn main() {
-    enum IpAddr {
-        V4(String),
-        V6(String),
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
+}
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+fn main() {
+    let q_fl = Coin::Quarter(UsState::Alaska);
 
-    let loopback = IpAddr::V6(String::from("::1"));
+    value_in_cents(q_fl);
 }
